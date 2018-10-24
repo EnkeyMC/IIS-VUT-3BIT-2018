09736b5b0e66623e19dddf75dab3d68b87812c2e
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2018-10-24 21:22:33 +0200

    Add bugtracker models to Django admin page

--- backend/bugtracker/admin.py
+++ backend/bugtracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bug, Language, Module, Patch, Profile, Severity, Ticket
 
-# Register your models here.
+admin.site.register(Bug)
+admin.site.register(Language)
+admin.site.register(Module)
+admin.site.register(Patch)
+admin.site.register(Profile)
+admin.site.register(Severity)
+admin.site.register(Ticket)
