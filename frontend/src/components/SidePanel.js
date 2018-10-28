18a8d0314fbfc83830112df8873642d88de4c125
Petr Hendrych <32193900+petrHendrych@users.noreply.github.com>
2018-10-28 22:45:43 +0100

    random

--- frontend/src/components/SidePanel.js
+++ frontend/src/components/SidePanel.js
@@ -6,22 +6,28 @@ export default class SidePanel extends Component {
     render() {
         return (
             <nav className="float-left side-panel">
-                <div className="w-100 mt-5">
-                    <div className="text-center pt-2">
+                <div className="w100 mt-5">
+                    <div className="icon-box text-center pt-2">
                         <FontAwesomeIcon icon ="check-circle" />
                     </div>
-                    {/*<div>*/}
-                        {/*sfd*/}
-                    {/*</div>*/}
-                </div>
-                <div className="w-100">
-                    <div className="text-center pt-2">
-                        <FontAwesomeIcon icon ="box-open" />
+                    <div className="icon-text">
+                        Done
                     </div>
                 </div>
-                <div className="w-100">
-                    <div className="text-center pt-2">
+                <div className="w100">
+                    <div className="icon-box text-center pt-2">
+                        <FontAwesomeIcon icon ="box-open" />
+                    </div>
+                    <div className="icon-text">
+                        Solving
+                    </div>
+                </div>
+                <div className="w100">
+                    <div className="icon-box text-center pt-2">
                         <FontAwesomeIcon icon ="user" />
+                        <div className="icon-text">
+                            My
+                        </div>
                     </div>
                 </div>
             </nav>
