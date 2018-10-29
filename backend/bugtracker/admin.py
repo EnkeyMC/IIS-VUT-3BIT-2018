16669ec8d5e5d512270d39c79140b1a2e3b91bf4
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2018-10-29 21:42:39 +0100

    Put user's profile inside Users page in admin site

--- backend/bugtracker/admin.py
+++ backend/bugtracker/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Bug, Language, Module, Patch, Profile, Severity, Ticket
 
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Bug)
 admin.site.register(Language)
 admin.site.register(Module)
 admin.site.register(Patch)
-admin.site.register(Profile)
 admin.site.register(Severity)
 admin.site.register(Ticket)
