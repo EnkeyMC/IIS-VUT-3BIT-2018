aa8b04e618b7c8f09f89420adfab4983ea220a6a
Petr Hendrych <32193900+petrHendrych@users.noreply.github.com>
2018-11-02 19:21:08 +0100

    Ticket info - part1

--- frontend/src/components/TicketInfo.js
+++ frontend/src/components/TicketInfo.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Col, Container, Row} from "reactstrap";
+import {Col, Container, Row, Media} from "reactstrap";
 
 
 export default class TicketList extends Component {
@@ -10,10 +10,18 @@ export default class TicketList extends Component {
                     <Row>
                         <Col md="8" xs="12">
                             <Container>
-                                <Row>adf</Row>
-                                <Row>ad</Row>
-                                <Row>df</Row>
-                                <Row>sf</Row>
+                                <Row className="pt-3">
+                                    <h1 className="display-4">Name of ticket</h1>
+                                </Row>
+                                <Row className="pt-3">
+                                    <Detail/>
+                                </Row>
+                                <Row className="pt-3">
+                                    <Description/>
+                                </Row>
+                                <Row className="pt-3">
+                                    <UploadFiles/>
+                                </Row>
                             </Container>
                         </Col>
                         <Col md="4" xs="12">
@@ -27,4 +35,46 @@ export default class TicketList extends Component {
             </div>
         );
     }
+}
+
+function Detail(props) {
+    return (
+        <Container className="no-margin mt-md-4">
+            <Media heading>Details</Media>
+            <Row>
+                <Col md="6" xs="12">
+                    <Row className="no-margin">Relevance:</Row>
+                    <Row className="no-margin">Vulnerability:</Row>
+                </Col>
+                <Col md="6" xs="12">
+                    <Row className="no-margin">State:</Row>
+                    <Row className="no-margin">Date:</Row>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+function Description(props) {
+    return (
+        <Media>
+            <Media body>
+                <Media heading>Description</Media>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
+                purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+                vulputate fringilla. Donec lacinia congue felis in faucibus.
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras
+                purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+                vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </Media>
+        </Media>
+    );
+}
+
+function UploadFiles(props) {
+    return (
+      <div className="w-100">
+          <Media heading>Uploaded files</Media>
+      </div>
+    );
 }
