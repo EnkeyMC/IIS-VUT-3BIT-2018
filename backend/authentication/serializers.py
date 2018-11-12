7279c4f3c62db8ea49a26513427d99742492df3b
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2018-11-12 14:54:53 +0100

    Fix token authentication

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
@@ -33,3 +34,15 @@ class UserLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'user_type')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError(
+                'Unable to log in with provided credentials.')
