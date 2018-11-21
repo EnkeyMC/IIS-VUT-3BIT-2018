9331c78320d9a5faac6e8d73d6bfe8df1ce6ad84
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2018-11-21 19:18:25 +0100

    Add ticket filtering by author or expert username

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -4,13 +4,15 @@ from bugtracker.models import Ticket, Module, Language
 
 
 class TicketFilter(filters.FilterSet):
+    username = filters.CharFilter(field_name='author__username')
+    expert = filters.CharFilter(field_name='expert__username')
     duplicate = filters.BooleanFilter(lookup_expr='isnull', exclude=True)
     status = filters.ChoiceFilter(choices=Ticket.STATUS_CHOICES)
     date = filters.DateFromToRangeFilter(field_name='created')
 
     class Meta:
         model = Ticket
-        fields = ['status', 'duplicate', 'date']
+        fields = ['status', 'duplicate', 'date', 'expert', 'username']
 
 
 class ModuleFilter(filters.FilterSet):
